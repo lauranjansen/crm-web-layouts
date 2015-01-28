d64aca74eb908eb057c5372757bd08abfdc6ae28
lauranjansen <lauran.jansen@gmail.com>
2015-01-28 14:48:38 -0500

    Add initial commit based on day 7

---
+++ crm.rb
@@ -0,0 +1,30 @@
+require_relative 'contact'
+require_relative 'rolodex'
+require 'sinatra'
+
+$rolodex = Rolodex.new
+
+get '/'  do
+	@crm_name = "My CRM"
+	erb :index
+end
+
+get '/contacts' do
+	@contacts = $rolodex.contacts
+  erb :contacts
+end
+
+# get '/contacts/:id' do
+
+# end
+
+get '/contacts/new_contact' do
+	erb :new_contact
+end
+
+post '/contacts' do
+	# p params: params
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+	$rolodex.add_contact(new_contact)
+	redirect '/contacts'
+end
